ed88fcbf5b8341f6c17748b25a8a9b2276395675
Ismat Babirli <i.babirli@outlook.com>
2021-05-17 21:37:15 +0400

    image pull secrets changed

--- tf/variables.tf
+++ tf/variables.tf
@@ -2,6 +2,6 @@ variable "DOCKER_IMG_TAG" {}
 variable "APP_NAME" {}
 
 locals {
-    repository    = "https://index.docker.io/v1"
+    repository    = "ismatbabir"
     service_image = "${local.repository}/${var.APP_NAME}:${var.DOCKER_IMG_TAG}"
 }
